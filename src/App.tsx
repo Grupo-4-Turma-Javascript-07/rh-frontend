a3a24e2022cee12f0a23fc8655d91f6d5e3d3405
Melissa <melissasilva245@gmail.com>
2025-08-06 16:08:34 -0300

    atualização na home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Home from './pages/home/Home'
+
 function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <Home />
+    )
 }
 
 export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,31 @@
+import RH from "../../assets/RH.svg"
+
+function Home() {
+    return (
+        <>
+            <div className=" min-h-screen bg-background flex justify-center">
+                <div className='container grid grid-cols-2  text-nav-footer'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold bg-'>
+                            RH Generation
+                        </h2>
+                        <p className='text-xl'>
+                            Gerencie sua equipe com mais eficiência, organização e praticidade.
+                            Aqui você encontra tudo o que precisa para administrar dados de colaboradores, acompanhar desempenho, controlar benefícios e muito mais — tudo em um só lugar.
+                        </p>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src={RH}
+                            alt="Imagem Página Home"
+                            className='w-3/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
