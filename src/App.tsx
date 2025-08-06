b057c9c0ad67d75b0f3de5f6df68f33bf28bd2ad
larissa <rissapins@gmail.com>
2025-08-06 18:48:53 -0300

    feat: cria Navbar responsiva, estrutura inicial das seções principais e scroll suave

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Servicos from "./pages/servicos/Servicos";
+import Sobre from "./pages/sobre/Sobre";
+
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <main className="pt-20">
+        <Home />
+        <Sobre />
+        <Servicos />
+      </main>
+    </>
   )
 }
 
---
+++ src/pages/sobre/Sobre.tsx
@@ -0,0 +1,11 @@
+function Sobre() {
+  return (
+    <div id="sobre" className="min-h-screen scroll-mt-20">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis quod explicabo voluptas et, repellendus nisi delectus necessitatibus enim maxime ipsa ratione nobis illo sit rem error. Recusandae ea iure iusto.
+      </p>
+    </div>
+  )
+}
+
+export default Sobre
