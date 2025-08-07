d4a2587bdda178b845ce3c4f0c9d58e6f2cd8dfa
user <user@users-MacBook-Pro.local>
2025-08-07 16:47:56 -0300

    modal adjust

--- src/components/sobre/modal/modal.tsx
+++ src/components/sobre/modal/modal.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react'
 import Popup from 'reactjs-popup'
 import 'reactjs-popup/dist/index.css';
-import './Modal.css'
+import './modal.css'
 
 interface modalProps {
   component: ReactNode
